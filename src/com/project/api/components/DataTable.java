5d288fa2ed92dbe94827bd93e447ae9729bebfb5
kddelosreyes <dr.kimhowel@gmail.com>
2017-10-02 18:08:29 +0800

    TableFieldManager
    TableFieldManager for DataTable columns

--- src/com/project/api/components/DataTable.java
+++ src/com/project/api/components/DataTable.java
@@ -5,11 +5,10 @@
  */
 package com.project.api.components;
 
-import com.project.api.models.Item;
-import com.project.api.translations.I18nUI;
-import com.project.api.translations.Translations;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -18,9 +17,18 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
+
+import com.project.api.models.Item;
+import com.project.api.oraclesql.Column;
+import com.project.api.translations.I18nUI;
+import com.project.api.translations.Translations;
+import com.project.api.utils.ClassUtils;
 
 /**
  *
@@ -28,145 +36,177 @@ import javafx.scene.layout.GridPane;
  */
 public class DataTable extends GridPane {
 
-    private TableView table;
-    private List<Item> items;
-    private Footer footer;
+	private TableView table;
+	private List<Item> items;
+	private Footer footer;
+	private List<TableColumn> tableColumns = new ArrayList<>();
 
-    public DataTable(List<Item> items) {
-        this.items = items;
-        table = new TableView();
-        footer = new Footer(table);
-    }
+	private final TableFieldManager tableFieldManager = new TableFieldManager();
 
-    private int getRecordsSize() {
-        return items.size();
-    }
+	public DataTable(List<Item> items) {
+		this.items = items;
+		table = new TableView();
+		footer = new Footer(table);
+	}
 
-    private class Footer extends GridPane {
+	private int getRecordsSize() {
+		return items.size();
+	}
 
-        private ObservableList<Integer> noOfRecords = FXCollections.observableArrayList(5, 10, 50, 100, 200, 100);
+	public void addTableColumn(Column column) {
+		Class<?> classDataType = ClassUtils.getWrapper(column);
 
-        private Button firstButton = new Button(I18nUI.getString(Translations.FIRST));
-        private Button previousButton = new Button(I18nUI.getString(Translations.PREVIOUS));
-        private TextField currentPageField = new TextField();
-        private Label totalPagesLabel = new Label();
-        private Button nextButton = new Button(I18nUI.getString(Translations.NEXT));
-        private Button lastButton = new Button(I18nUI.getString(Translations.LAST));
-        private Label entriesLabel = new Label(I18nUI.getString(Translations.SHOWING_OUT_OF_ENTRIES));
-        private Label pageSizeLabel = new Label(I18nUI.getString(Translations.PAGE_SIZE));
-        private ComboBox pageOptions = new ComboBox(noOfRecords);
+		if (String.class.equals(classDataType)) {
+			tableColumns.add(tableFieldManager.getStringTableColumn(column
+					.getColumnName()));
+		} else if (Character.class.equals(classDataType)) {
+			tableColumns.add(tableFieldManager.getCharacterTableColumn(column
+					.getColumnName()));
+		} else if (Double.class.equals(classDataType)) {
+			tableColumns.add(tableFieldManager.getDoubleTableColumn(column
+					.getColumnName()));
+		} else if (Long.class.equals(classDataType)) {
+			tableColumns.add(tableFieldManager.getLongTableColumn(column
+					.getColumnName()));
+		} else if (Date.class.equals(classDataType)) {
+			tableColumns.add(tableFieldManager.getDateTableColumn(column
+					.getColumnName()));
+		}
 
-        private final int DEFAULT_NO_OF_RECORDS = 50;
+	}
 
-        private TableView table;
-        private int currentPage = 1;
+	private class Footer extends GridPane {
 
-        public Footer(TableView table) {
-            this.table = table;
-            init();
-        }
+		private ObservableList<Integer> noOfRecords = FXCollections
+				.observableArrayList(5, 10, 50, 100, 200, 100);
 
-        private void init() {
-            firstButton.setOnAction((ActionEvent e) -> {
-                currentPage = 1;
-                setCurrentPage(currentPage);
-                loadTableContent();
+		private Button firstButton = new Button(
+				I18nUI.getString(Translations.FIRST));
+		private Button previousButton = new Button(
+				I18nUI.getString(Translations.PREVIOUS));
+		private TextField currentPageField = new TextField();
+		private Label totalPagesLabel = new Label();
+		private Button nextButton = new Button(
+				I18nUI.getString(Translations.NEXT));
+		private Button lastButton = new Button(
+				I18nUI.getString(Translations.LAST));
+		private Label entriesLabel = new Label(
+				I18nUI.getString(Translations.SHOWING_OUT_OF_ENTRIES));
+		private Label pageSizeLabel = new Label(
+				I18nUI.getString(Translations.PAGE_SIZE));
+		private ComboBox<Integer> pageOptions = new ComboBox<>(noOfRecords);
 
-                loadButtonState(true,
-                        true,
-                        currentPage == getCurrentTotalPages(),
-                        currentPage == getCurrentTotalPages());
-            });
-            add(firstButton, 0, 0);
+		private final int DEFAULT_NO_OF_RECORDS = 50;
 
-            previousButton.setOnAction((ActionEvent e) -> {
-                currentPage--;
-                setCurrentPage(currentPage);
+		private TableView<Item> table;
+		private int currentPage = 1;
 
-                loadButtonState(currentPage == 1,
-                        currentPage == 1,
-                        false,
-                        false);
-            });
-            add(previousButton, 1, 0);
+		public Footer(TableView<Item> table) {
+			this.table = table;
+			init();
+		}
 
-            add(currentPageField, 2, 0);
+		private void init() {
+			firstButton.setOnAction((ActionEvent e) -> {
+				currentPage = 1;
+				setCurrentPage(currentPage);
+				loadTableContent();
 
-            add(totalPagesLabel, 3, 0);
+				loadButtonState(true, true,
+						currentPage == getCurrentTotalPages(),
+						currentPage == getCurrentTotalPages());
+			});
+			add(firstButton, 0, 0);
 
-            nextButton.setOnAction((ActionEvent e) -> {
-                currentPage++;
-                setCurrentPage(currentPage);
-                
-                loadButtonState(false,
-                        false,
-                        currentPage == getCurrentTotalPages(),
-                        currentPage == getCurrentTotalPages());
-            });
-            add(nextButton, 4, 0);
+			previousButton.setOnAction((ActionEvent e) -> {
+				currentPage--;
+				setCurrentPage(currentPage);
 
-            lastButton.setOnAction((ActionEvent e) -> {
-                currentPage = getCurrentTotalPages();
-                setCurrentPage(currentPage);
+				loadButtonState(currentPage == 1, currentPage == 1, false,
+						false);
+			});
+			add(previousButton, 1, 0);
 
-                loadButtonState(true,
-                        true,
-                        currentPage == getCurrentTotalPages(),
-                        currentPage == getCurrentTotalPages());
-            });
-            add(lastButton, 5, 0);
-            
-            add(entriesLabel, 6, 0);
-            add(pageSizeLabel, 7, 0);
-            
-            pageOptions.valueProperty().addListener(new ChangeListener<Integer>() {
-                @Override
-                public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
-                    
-                }
-            });
-            add(pageOptions, 8, 0);
+			add(currentPageField, 2, 0);
 
-            setDefaultState();
-        }
+			add(totalPagesLabel, 3, 0);
 
-        private void loadTableContent() {
-            List<Item> temp = new ArrayList<>();
-            for (int i = (currentPage - 1) * getCurrentPageSize(); i < currentPage * getCurrentPageSize(); i++) {
-                if (items.get(i) != null) {
-                    temp.add(items.get(i));
-                } else {
-                    break;
-                }
-            }
-            table.setItems(FXCollections.observableList(temp));
-        }
+			nextButton.setOnAction((ActionEvent e) -> {
+				currentPage++;
+				setCurrentPage(currentPage);
 
-        private int getCurrentPageSize() {
-            return Integer.parseInt(pageOptions.getValue().toString());
-        }
+				loadButtonState(false, false,
+						currentPage == getCurrentTotalPages(),
+						currentPage == getCurrentTotalPages());
+			});
+			add(nextButton, 4, 0);
 
-        private int getCurrentTotalPages() {
-            return Integer.parseInt(totalPagesLabel.getText());
-        }
+			lastButton.setOnAction((ActionEvent e) -> {
+				currentPage = getCurrentTotalPages();
+				setCurrentPage(currentPage);
 
-        private void setCurrentPage(int currPage) {
-            currentPageField.setText(String.valueOf(currPage));
-            loadTableContent();
-        }
+				loadButtonState(true, true,
+						currentPage == getCurrentTotalPages(),
+						currentPage == getCurrentTotalPages());
+			});
+			add(lastButton, 5, 0);
 
-        private void setDefaultState() {
-            loadButtonState(true, true, true, true);
-            pageOptions.setValue(DEFAULT_NO_OF_RECORDS);
-            setCurrentPage(1);
-        }
+			add(entriesLabel, 6, 0);
+			add(pageSizeLabel, 7, 0);
 
-        private void loadButtonState(boolean first, boolean previous, boolean next, boolean last) {
-            firstButton.setDisable(first);
-            previousButton.setDisable(previous);
-            nextButton.setDisable(next);
-            lastButton.setDisable(last);
-        }
+			pageOptions.valueProperty().addListener(
+					new ChangeListener<Integer>() {
+						@Override
+						public void changed(
+								ObservableValue<? extends Integer> observable,
+								Integer oldValue, Integer newValue) {
 
-    }
+						}
+					});
+			add(pageOptions, 8, 0);
+
+			setDefaultState();
+		}
+
+		private void loadTableContent() {
+			List<Item> temp = new ArrayList<>();
+			for (int i = (currentPage - 1) * getCurrentPageSize(); i < currentPage
+					* getCurrentPageSize(); i++) {
+				if (items.get(i) != null) {
+					temp.add(items.get(i));
+				} else {
+					break;
+				}
+			}
+			table.setItems(FXCollections.observableList(temp));
+		}
+
+		private int getCurrentPageSize() {
+			return Integer.parseInt(pageOptions.getValue().toString());
+		}
+
+		private int getCurrentTotalPages() {
+			return Integer.parseInt(totalPagesLabel.getText());
+		}
+
+		private void setCurrentPage(int currPage) {
+			currentPageField.setText(String.valueOf(currPage));
+			loadTableContent();
+		}
+
+		private void setDefaultState() {
+			loadButtonState(true, true, true, true);
+			pageOptions.setValue(DEFAULT_NO_OF_RECORDS);
+			setCurrentPage(1);
+		}
+
+		private void loadButtonState(boolean first, boolean previous,
+				boolean next, boolean last) {
+			firstButton.setDisable(first);
+			previousButton.setDisable(previous);
+			nextButton.setDisable(next);
+			lastButton.setDisable(last);
+		}
+
+	}
 }
